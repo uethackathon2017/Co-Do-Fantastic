7d4cb2ca9797f596d55c69d86cf435d9bd6873b0
Anh Dao Tuan <Anh Dao Tuan>
2017-03-11 02:11:11 +0700

    layout list alarm

---
+++ app/src/main/java/com/example/anhdt/smartalarm/SetAlarmActivity.java
@@ -0,0 +1,13 @@
+package com.example.anhdt.smartalarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SetAlarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_alarm);
+    }
+}
