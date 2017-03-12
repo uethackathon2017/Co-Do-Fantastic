3ef6c616a13ff58f8c23895cbb690e86eadf0618
Anh Dao Tuan <Anh Dao Tuan>
2017-03-12 08:54:56 +0700

    6.0

--- app/src/main/java/com/example/anhdt/smartalarm/adapters/PagerAdapter.java
+++ app/src/main/java/com/example/anhdt/smartalarm/adapters/PagerAdapter.java
@@ -24,9 +24,8 @@ import com.example.anhdt.smartalarm.services.GPSTracker;
 public class PagerAdapter extends FragmentStatePagerAdapter{
     int mNumOfTabs;
     private Context context;
-    private GPSTracker gpsTracker;
 
-    public PagerAdapter(FragmentManager fm, int NumOfTabs,Context context) {
+    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
         super(fm);
         this.mNumOfTabs = NumOfTabs;
         this.context = context;
@@ -39,7 +38,6 @@ public class PagerAdapter extends FragmentStatePagerAdapter{
             case 0:
                 return new MainFragment();
             case 1:
-                //getLatLong();
                 return new NewsFragment();
             default:
                 return null;
@@ -57,36 +55,9 @@ public class PagerAdapter extends FragmentStatePagerAdapter{
             case 0:
                 return "Báo thức";
             case 1:
-                //getLatLong();
                 return "Thời tiết & Tin tức";
             default:
                 return null;
         }
     }
-//    @Override
-//    public void setOnShowSettingIntent() {
-//        context.startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-//    }
-
-//    private void getLatLong() {
-//
-//        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            ActivityCompat.requestPermissions((Activity)context, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-//
-//        } else {
-//            gpsTracker = new GPSTracker(context, (Activity)context, this);
-//
-//            // Check if GPS enabled
-//            if (gpsTracker.canGetLocation()) {
-//
-//                double latitude = gpsTracker.getLatitude();
-//                double longitude = gpsTracker.getLongitude();
-//            } else {
-//                // Can't get location.
-//                // GPS or network is not enabled.
-//                // Ask user to enable GPS/network in settings.
-//                gpsTracker.showSettingsAlert();
-//            }
-//        }
-//    }
 }
